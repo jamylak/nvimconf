6b16dcea4c0cfa1178c8cb2bd794069b62df5927
jamylak <jamylak@gmail.com>
2024-03-05 09:35:57 +1100

    Add oil

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		default_file_explorer = true,
+		columns = {
+			"icon",
+			"permissions",
+			"size",
+			"mtime",
+		},
+		keymaps = {
+			["cd"] = "actions.cd",
+		},
+	},
+}
