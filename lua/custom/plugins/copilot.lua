117ca22e4b9b2e508bac0aa32235f7c9d277d8b2
jamylak <jamylak@gmail.com>
2024-03-06 23:38:49 +1100

    Use a better coplit

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,1 +1,20 @@
-return { 'github/copilot.vim', opts = {}, config = function() end }
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = '<C-CR>',
+        accept_word = false,
+        accept_line = false,
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-]>',
+      },
+    },
+  },
+}
