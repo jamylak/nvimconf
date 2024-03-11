a0955aaa955267daaf4a4e04b9ca9075cf4249fc
jamylak <jamylak@gmail.com>
2024-03-11 15:58:57 +1100

    Change keybindings for visual multi to not clash with smart splits

--- lua/custom/plugins/visual_multi.lua
+++ lua/custom/plugins/visual_multi.lua
@@ -1,10 +1,10 @@
 -- Not playing well with lua snips eg. CTRL-Y not working
-vim.g.VM_leader = "=" -- sets vim.g.VM_leader for visual multi
+vim.g.VM_leader = '=' -- sets vim.g.VM_leader for visual multi
 return {
-	"mg979/vim-visual-multi",
-	branch = "master",
-	config = function()
-		vim.keymap.set("n", "<A-j>", "<Plug>(VM-Select-Cursor-Down)")
-		vim.keymap.set("n", "<A-k>", "<Plug>(VM-Select-Cursor-Up)")
-	end,
+  'mg979/vim-visual-multi',
+  branch = 'master',
+  config = function()
+    vim.keymap.set('n', '<C-j>', '<Plug>(VM-Select-Cursor-Down)')
+    vim.keymap.set('n', '<C-k>', '<Plug>(VM-Select-Cursor-Up)')
+  end,
 }
