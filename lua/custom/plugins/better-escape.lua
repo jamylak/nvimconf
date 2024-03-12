ed8d2d06812ee8accf0d670dcac22980bd908ac9
jamylak <jamylak@gmail.com>
2024-03-13 09:31:20 +1100

    better escape bindings update

--- lua/custom/plugins/better-escape.lua
+++ lua/custom/plugins/better-escape.lua
@@ -1,7 +1,7 @@
 return {
   'max397574/better-escape.nvim',
   opts = {
-    mapping = { 'fj', 'fh', 'hf', 'wd', 'dw', 'ji', 'jk', 'jj', 'ko' }, -- a table with mappings to use
+    mapping = { 'dh', 'fj', 'fh', 'hf', 'wd', 'dw', 'ji', 'jk', 'jj', 'ko' }, -- a table with mappings to use
     timeout = 150, -- the time in which the keys must be hit in ms.
     clear_empty_lines = false, -- clear line after escaping if there is only whitespace
     keys = '<Esc>', -- keys used for escaping, if it is a function will use the result everytime
