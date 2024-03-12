51c800cfcb775134c097412bd70873514b346841
jamylak <jamylak@gmail.com>
2024-03-13 09:31:55 +1100

    sg-nvim testing

---
+++ lua/custom/plugins/sg-nvim.lua
@@ -0,0 +1,13 @@
+return {
+  -- {
+  --   'sourcegraph/sg.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim', --[[ "nvim-telescope/telescope.nvim ]]
+  --   },
+  --   config = function()
+  --     -- Keymap for <leader>sg
+  --     vim.api.nvim_set_keymap('n', '<leader>sg', '<cmd>lua require("sg.extensions.telescope").fuzzy_search_results()<CR>', { noremap = true, silent = true })
+  --     require('sg').setup {}
+  --   end,
+  -- },
+}
