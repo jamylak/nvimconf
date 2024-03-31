1e5013d4173dab19e6f1dfb07ea5638c409ab30a
jamylak <jamylak@gmail.com>
2024-03-31 23:22:11 +1100

    focus terminal after window jump

--- lua/custom/plugins/nvim-window.lua
+++ lua/custom/plugins/nvim-window.lua
@@ -2,7 +2,8 @@ vim.keymap.set('i', 'gj', "<esc><cmd>lua require('nvim-window').pick()<cr>", { d
 vim.keymap.set('t', 'gj', "<C-\\><C-n><cmd>lua require('nvim-window').pick()<cr>", { desc = 'nvim-window: Jump to window', silent = true })
 
 return {
-  'yorickpeterse/nvim-window',
+  'jamylak/nvim-window',
+  branch = 'feature/after-jump-callback',
   keys = {
     -- { '<leader>wj', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'nvim-window: Jump to window' },
     { 'gj', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'nvim-window: Jump to window' },
@@ -33,6 +34,13 @@ return {
         'y',
         'z',
       },
+      after_jump_callback = function(window)
+        local buf_id = vim.api.nvim_win_get_buf(window)
+        local buf_type = vim.api.nvim_buf_get_option(buf_id, 'buftype')
+        if buf_type == 'terminal' then
+          vim.cmd 'startinsert'
+        end
+      end,
     }
   end,
 }
