9da1ff6d3901d9f2a6ace9d1d973fa318f866ce6
jamylak <jamylak@gmail.com>
2024-04-01 20:21:57 +1100

    Better after jump actions

--- lua/custom/plugins/nvim-window.lua
+++ lua/custom/plugins/nvim-window.lua
@@ -2,11 +2,21 @@ vim.keymap.set('i', 'gj', "<esc><cmd>lua require('nvim-window').pick()<cr>", { d
 vim.keymap.set('t', 'gj', "<C-\\><C-n><cmd>lua require('nvim-window').pick()<cr>", { desc = 'nvim-window: Jump to window', silent = true })
 
 return {
-  'jamylak/nvim-window',
+  'yorickpeterse/nvim-window',
   branch = 'feature/after-jump-callback',
   keys = {
-    -- { '<leader>wj', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'nvim-window: Jump to window' },
-    { 'gj', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'nvim-window: Jump to window' },
+    {
+      'gj',
+      function()
+        require('nvim-window').pick()
+        local buf_id = vim.api.nvim_get_current_buf()
+        local buf_type = vim.api.nvim_buf_get_option(buf_id, 'buftype')
+        if buf_type == 'terminal' then
+          vim.cmd 'startinsert'
+        end
+      end,
+      desc = 'nvim-window: Jump to window',
+    },
   },
   config = function()
     require('nvim-window').setup {
@@ -34,13 +44,6 @@ return {
         'y',
         'z',
       },
-      after_jump_callback = function(window)
-        local buf_id = vim.api.nvim_win_get_buf(window)
-        local buf_type = vim.api.nvim_buf_get_option(buf_id, 'buftype')
-        if buf_type == 'terminal' then
-          vim.cmd 'startinsert'
-        end
-      end,
     }
   end,
 }
