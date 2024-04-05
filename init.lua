774f2685f2c14345bf42859e3e4d0a900ad47079
jamylak <jamylak@gmail.com>
2024-04-05 14:09:19 +1100

    Symbol workspaces remapped

--- init.lua
+++ init.lua
@@ -536,7 +536,7 @@ require('lazy').setup {
 
           -- Fuzzy find all the symbols in your current workspace
           --  Similar to document symbols, except searches over your whole project.
-          map('<leader>sw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[S]ymbols: [W]orkspace')
+          map('<leader>ss', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[S]ymbols: [W]orkspace')
 
           -- Rename the variable under your cursor
           --  Most Language Servers support renaming across files, etc.
