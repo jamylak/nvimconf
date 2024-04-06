624a5dfe59ed5d97d56ab95df49dac5f8ae7d172
jamylak <jamylak@gmail.com>
2024-04-06 23:12:29 +1100

    neovide settings

--- init.lua
+++ init.lua
@@ -56,20 +56,21 @@ vim.opt.timeoutlen = 200
 -- Neovide settings
 if vim.g.neovide then
   -- vim.api.nvim_set_keymap('n', '<D-v>', '"*p', { noremap = true })
-  vim.o.guifont = 'MesloLGS NF:h12'
+  vim.o.guifont = 'MesloLGS NF:h12.5'
   -- Set current working directory to the project directory env var
   local projects_dir = os.getenv 'PROJECTS_DIR'
   vim.cmd('cd ' .. projects_dir)
-  vim.api.nvim_set_keymap('i', '<D-v>', '<cmd>normal "*P<cr>', { noremap = true })
+  -- vim.api.nvim_set_keymap('i', '<D-v>', '<cmd><ESC> "*P<cr>', { noremap = true })
+  vim.keymap.set('i', '<D-v>', '<C-r>*') -- Paste insert mode
+  vim.keymap.set('t', '<D-v>', '<C-\\><C-n>l"+Pli') -- Paste insert mode
   vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
   vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
   vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
   vim.g.neovide_input_macos_alt_is_meta = true
   vim.o.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
 end
--- vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 vim.g.neovide_scroll_animation_length = 0.05
-vim.g.neovide_transparency = 0.87
+vim.g.neovide_transparency = 0.7
 vim.g.neovide_window_blurred = true
 vim.g.neovide_show_border = true -- Doesn't seem to do anything
 vim.g.neovide_hide_mouse_when_typing = true
