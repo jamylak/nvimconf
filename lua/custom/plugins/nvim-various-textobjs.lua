57b5487a7598dc08c67aacdcca7512ebae1c04e7
jamylak <jamylak@gmail.com>
2024-04-07 21:02:15 +1000

    in -> id on nvim-various-textobjs

--- lua/custom/plugins/nvim-various-textobjs.lua
+++ lua/custom/plugins/nvim-various-textobjs.lua
@@ -1,5 +1,10 @@
+local keymap = vim.keymap.set
+keymap({ 'o', 'x' }, 'id', "<cmd>lua require('various-textobjs').number('inner')<CR>")
+keymap({ 'o', 'x' }, 'ad', "<cmd>lua require('various-textobjs').number('outer')<CR>")
+
 return {
   'chrisgrieser/nvim-various-textobjs',
   event = 'BufReadPost',
   opts = { useDefaultKeymaps = true },
+  disabledKeymaps = { 'in', 'an' },
 }
