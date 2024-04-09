26f56b648a5463c9a30fd022657e1a9e62cbe8f7
jamylak <jamylak@gmail.com>
2024-04-09 10:21:42 +1000

    example query

--- queries/cpp/expression.scm
+++ queries/cpp/expression.scm
@@ -2,3 +2,7 @@
 (field_expression) @expression
 (call_expression) @expression
 (binary_expression) @expression
+; https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+; Example how to get rhs
+; (assignment_expression
+;   right: (_) @testrhs)
