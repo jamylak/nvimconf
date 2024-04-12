625b5117d072d7884a7dfc8beeec1a852ca89cf2
jamylak <jamylak@gmail.com>
2024-04-12 16:16:00 +1000

    Simplify build since we don't use windows

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -6,15 +6,7 @@ return {
     -- Snippet Engine & its associated nvim-cmp source
     {
       'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets
-        -- This step is not supported in many windows environments
-        -- Remove the below condition to re-enable on windows
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
+      build = 'make install_jsregexp',
     },
 
     'onsails/lspkind.nvim',
