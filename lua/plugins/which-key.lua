ac597a998b4f095364b579ed1c3b8c090c0fecb7
jamylak <jamylak@gmail.com>
2024-04-12 16:16:42 +1000

    Update which key to ignore some stuff so it doesnt break it

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,12 +1,13 @@
 return {
   'folke/which-key.nvim',
-  keys = { '<leader>', 'a', 'b', 'c', 'g', 'h', 'l', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z', ']', '[' },
+  keys = { '<leader>', 'a', 'c', 'g', 'h', 'l', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'x', 'y', 'z', ']', '[' },
   config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup {
       triggers_blacklist = {
         -- list of mode / prefixes that should never be hooked by WhichKey
         -- this is mostly relevant for keymaps that start with a native binding
         i = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'f', 'i', '9', '8', 'w', 'd' },
+        n = { 'w', 'W', 'b', 'B' },
         v = { 'j', 'k' },
       },
     }
