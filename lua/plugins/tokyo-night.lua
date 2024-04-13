50febf4d0a7123876f0f978d0baa330370470cc1
jamylak <jamylak@gmail.com>
2024-04-13 13:37:20 +1000

    turn off tokyo night temporarily

--- lua/plugins/tokyo-night.lua
+++ lua/plugins/tokyo-night.lua
@@ -1,12 +1,14 @@
-return {
-  'folke/tokyonight.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- Load the colorscheme here
-    vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
+vim.cmd 'colorscheme darkblue'
+return {}
+-- return {
+--   'folke/tokyonight.nvim',
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     -- Load the colorscheme here
+--     vim.cmd.colorscheme 'tokyonight-night'
+--
+--     -- You can configure highlights by doing something like
+--     vim.cmd.hi 'Comment gui=none'
+--   end,
+-- }
