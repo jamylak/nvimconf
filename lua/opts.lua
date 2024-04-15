4c5d8a035673010c5be827d31379a60e288aa9de
jamylak <jamylak@gmail.com>
2024-04-16 07:40:52 +1000

    Update list chars

--- lua/opts.lua
+++ lua/opts.lua
@@ -48,11 +48,8 @@ vim.opt.timeoutlen = 200
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Sets how neovim will display certain whitespace in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '» ', nbsp = '␣', space = '·', trail = '-', eol = '↲', extends = '▶', precedes = '◀' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
