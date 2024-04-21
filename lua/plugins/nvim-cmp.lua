9150b8333ff06d314c6b0b3af34c473dfa807d62
jamylak <jamylak@gmail.com>
2024-04-22 08:42:24 +1000

    Update C-j for nvim-cmp to jump next

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -119,8 +119,8 @@ return {
           end
         end, { 'i', 's' }),
         ['<C-j>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
           end
         end, { 'i', 's' }),
       },
