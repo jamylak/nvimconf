e54bb010abf4db9303339f0a550475aa49e6c80b
jamylak <jamylak@gmail.com>
2024-08-24 13:36:17 +1000

    GLSL test snippet

--- lua/snippets/glsl.lua
+++ lua/snippets/glsl.lua
@@ -6,6 +6,16 @@ local s = ls.snippet
 local i = ls.insert_node
 
 ls.add_snippets('glsl', {
+  s('v2', fmta('vec2 ', {})),
+  s(
+    'vk',
+    fmta('vec2(<a>.<b>, <c>.<d>);', {
+      a = i(1),
+      b = i(2),
+      c = i(3),
+      d = i(4),
+    })
+  ),
   s(
     'v',
     fmta('vec3 <name> = vec3(<args>);', {
