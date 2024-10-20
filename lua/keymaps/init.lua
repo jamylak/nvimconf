13847c10e575bfccfebf21721ee0df0dbab07bd7
jamylak <jamylak@gmail.com>
2024-10-20 19:59:13 +1100

    C-c to toggle comment

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -254,8 +254,10 @@ end, { silent = true })
 
 -- Faster comment line
 vim.api.nvim_set_keymap('n', 'co', 'gcc', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-c>', 'gcc', { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>c', 'gcc', { silent = true })
 vim.api.nvim_set_keymap('v', '<leader>c', 'gc', { silent = true })
+vim.api.nvim_set_keymap('v', '<C-c>', 'gc', { silent = true })
 
 local changeDir = function()
   local file_path = vim.fn.expand '%:p'
