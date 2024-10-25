1335ddd348a4bc9d6ff68081c376774a3aadf962
jamylak <jamylak@gmail.com>
2024-10-26 07:23:53 +1100

    be lazier with nvimdadbod

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -3,7 +3,7 @@ return {
     'tpope/vim-dadbod',
     cmd = { 'DBUI', 'DB', 'DBUIToggle', 'DBUIFindBuffer', 'DBUIRenameBuffer' },
     dependencies = {
-      { 'kristijanhusak/vim-dadbod-completion', event = 'InsertEnter' },
+      { 'kristijanhusak/vim-dadbod-completion', cmd = { 'DBUI', 'DBUIToggle', 'DBUIFindBuffer', 'DBUIRenameBuffer' } },
       { 'kristijanhusak/vim-dadbod-ui', cmd = { 'DBUI', 'DBUIToggle', 'DBUIFindBuffer', 'DBUIRenameBuffer' } },
     },
     config = function()
