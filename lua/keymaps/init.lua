4ea890b9b5135a56e514d04f5b2822178e317a75
jamylak <jamylak@gmail.com>
2025-03-10 10:54:56 +1100

    Fix jk and ji navigation in lazygit and yazi

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -203,10 +203,17 @@ vim.keymap.set('n', '<leader>gm', cdOpenLazyGitFloating, { noremap = true })
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { silent = true })
 
-if not vim.g.neovide then
-  vim.keymap.set('t', 'jk', '<C-\\><C-n>', { silent = true })
-  vim.keymap.set('t', 'ji', '<C-\\><C-n>', { silent = true })
-end
+-- Allow jk and ji escape in terminal mode but not in yazi or lazygit
+-- to allow easy navigation
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    if not vim.api.nvim_buf_get_name(0):match 'yazi' and not vim.api.nvim_buf_get_name(0):match 'lazygit' then
+      vim.keymap.set('t', 'jk', '<C-\\><C-n>', { buffer = true, silent = true })
+      vim.keymap.set('t', 'ji', '<C-\\><C-n>', { buffer = true, silent = true })
+    end
+  end,
+})
 
 -- Testing escape keys
 local mapping = { 'ji', 'jk' }
