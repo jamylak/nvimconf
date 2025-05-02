c19d5d8141d0e6de425b61b5aef40528ec2667bc
jamylak <jamylak@gmail.com>
2025-05-03 09:10:41 +1000

    telescope cmdline

---
+++ lua/plugins/telescope-cmdline.lua
@@ -0,0 +1,20 @@
+return {
+  'jonarrien/telescope-cmdline.nvim',
+  dependencies = {},
+  cmd = 'Telescope cmdline',
+  keys = {
+    { '<m-space>', '<cmd>Telescope cmdline<cr>', desc = 'Telescope cmdline', mode = 'n' },
+    { '<m-space>', '<cmd>Telescope cmdline<cr>', desc = 'Telescope cmdline', mode = 'i' },
+  },
+  config = function()
+    require('telescope').load_extension 'cmdline'
+    require('telescope._extensions.cmdline').setup {
+      picker = {
+        layout_config = {
+          width = 120,
+          height = 25,
+        },
+      },
+    }
+  end,
+}
