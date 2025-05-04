a59fe53c857393e124cd5d940f4a7308165bce54
jamylak <jamylak@gmail.com>
2025-05-04 15:26:55 +1000

    fix that had been messing up rust lsp for a long time

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -93,7 +93,7 @@ return {
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.offsetEncoding = 'utf-8'
+    capabilities.offsetEncoding = { 'utf-8' }
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     -- Enable the following language servers
