e22e13e78f5af5dc69a96a935e55f11368f656dc
jamylak <jamylak@gmail.com>
2025-05-04 19:30:51 +1000

    unlock telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -57,7 +57,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
     '<leader>f.',
     '<leader>f,',
   },
-  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for install instructions
