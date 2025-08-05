352c3753681a2df9a2d5b31bf57e9413e9f069b5
jamylak <jamylak@gmail.com>
2025-08-05 18:56:26 +1000

    cmp-dap

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -23,6 +23,7 @@ return {
 
     'onsails/lspkind.nvim',
     'saadparwaiz1/cmp_luasnip',
+    "rcarriga/cmp-dap",
 
     -- Adds other completion capabilities.
     --  nvim-cmp does not ship with all sources by default. They are split
@@ -83,15 +84,22 @@ return {
     -- })
 
     cmp.setup {
+      -- Normally, nvim-cmp disables itself inside "prompt" buffers to avoid interfering with UIs like Telescope prompts.
+      -- But the DAP REPL is a "prompt" buffer — and we do want completions there.
+      -- So this line re-enables it only when the buffer is related to debugging.
+      enabled = function()
+        return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+            or require("cmp_dap").is_dap_buffer()
+      end,
       formatting = {
         fields = { 'abbr', 'kind', 'menu' },
         expandable_indicator = true,
         format = lspkind.cmp_format {
           mode = 'symbol', -- show only symbol annotations
-          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
           -- can also be a function to dynamically calculate max width such as
           -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
           show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
           -- The function below will be called before any actual modifications from lspkind
@@ -222,6 +230,11 @@ return {
       sources = {
         { name = 'vim-dadbod-completion' },
         { name = 'buffer' },
+      },
+    })
+    cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+      sources = {
+        { name = "dap" },
       },
     })
   end,
