265889f56c38532582a7c88e3403e464487e564c
jamylak <jamylak@gmail.com>
2025-08-09 08:28:52 +1000

    almost editing cmdline

--- lua/plugins/telescope-cmdline.lua
+++ lua/plugins/telescope-cmdline.lua
@@ -1,5 +1,6 @@
 return {
-  'jonarrien/telescope-cmdline.nvim',
+  'jamylak/telescope-cmdline.nvim',
+  branch = 'cmdline-edit',
   dependencies = {},
   cmd = 'Telescope cmdline',
   keys = {
@@ -15,6 +16,37 @@ return {
           width = 120,
           height = 25,
         },
+        -- These both fail
+        -- I want a way eventually to do something like
+        -- :<c-f>
+        attach_mappings = function(prompt_bufnr, map)
+          map('i', '<C-f>', function()
+            -- Get the current input from the prompt buffer
+            local current_input = vim.api.nvim_buf_get_lines(prompt_bufnr, 0, -1, false)[1] or ""
+
+            print("Current input: " .. current_input)
+
+
+            -- Close the Telescope picker
+            require('telescope.actions').close(prompt_bufnr)
+
+            -- Construct the command to feed: enter command mode, type the input, then literally press <C-f>
+            -- local cmd_to_feed = current_input .. "<C-f>"
+            local cmd_to_feed = current_input
+            --
+            -- Feed the keys to Neovim
+            -- 'n' for normal mode (to ensure we enter command mode), 'x' for execute, true for remap
+            vim.api.nvim_feedkeys(cmd_to_feed, 'nx', true)
+          end)
+          return true
+        end,
+        -- mappings = {
+        --   i = {
+        --     ["<C-f>"] = function(prompt_bufnr)
+        --       print("hello world")
+        --     end,
+        --   },
+        -- },
       },
     }
   end,
