56ae9a19efe6e06b901c3349541e3e494de2ffb9
jamylak <jamylak@gmail.com>
2025-08-10 19:22:43 +1000

    non lazy for now

--- lua/plugins/cppplug.lua
+++ lua/plugins/cppplug.lua
@@ -3,8 +3,11 @@ return {
     -- load from local directory sitting inside your config
     dir = vim.fn.stdpath("config") .. "/lua/cppplug",
     name = "myplugin",
+    lazy = false,
     -- lazy-load examples (optional):
-    cmd = { "CMakeListsTxtGenCPP", "CMakeListsTxtGenC" },
+    -- cmd = {
+    --   "CMakeListsTxtGenCPP", "CMakeListsTxtGenC"
+    -- },
     -- keys = { { "<leader>mh", function() require("cppplug").echo() end, desc = "My hello" } },
     opts = {},
     config = function(_, opts)
