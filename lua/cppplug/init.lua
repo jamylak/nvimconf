1a2611bc7e91fc8cdc69422a21cee3a2ecefb553
jamylak <jamylak@gmail.com>
2025-08-10 19:56:50 +1000

    template fixups

--- lua/cppplug/init.lua
+++ lua/cppplug/init.lua
@@ -1,9 +1,8 @@
 local M = {}
 
 M.opts = {
-  cpp_standard = 23, -- default C++ standard
-  c_standard = 17,   -- default C standard
-  project_name = "MyProject"
+  default_cpp_standard = 23,
+  default_c_standard = 17,
 }
 
 
@@ -13,41 +12,58 @@ local function write_file(path, content)
   fd:close()
 end
 
+local function get_safe_project_name()
+  local cwd = vim.fn.getcwd()
+  local project_folder_name = vim.fn.fnamemodify(cwd, ":t")
+  -- Sanitize the project name: replace spaces with underscores, remove non-alphanumeric (and non-underscore) characters
+  project_folder_name = project_folder_name:gsub("%s", "_"):gsub("[^%w_]", "")
+  return project_folder_name
+end
+
 local function gen_cpp()
   local content = [[
 cmake_minimum_required(VERSION 3.20)
-project(MyProject LANGUAGES CXX)
+project({{project_name}} LANGUAGES CXX)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD {{default_cpp_standard}})
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-add_executable(${PROJECT_NAME} main.cpp)
+add_executable({{project_name}} main.cpp)
 ]]
-  write_file("CMakeLists.txt", content)
-  vim.notify("Generated C++23 CMakeLists.txt in " .. vim.fn.getcwd())
+  local current_vars = vim.tbl_deep_extend("force", {}, M.opts)
+  current_vars.project_name = get_safe_project_name()
+  local formatted_content = content:gsub("{{(.-)}}", function(key)
+    return tostring(current_vars[key] or "")
+  end)
+  write_file("CMakeLists.txt", formatted_content)
+  vim.notify("Generated C++ CMakeLists.txt in " .. vim.fn.getcwd())
 end
 
 local function gen_c()
   local content = [[
 cmake_minimum_required(VERSION 3.20)
-project(MyProject LANGUAGES C)
+project({{project_name}} LANGUAGES C)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-set(CMAKE_C_STANDARD 17)
+set(CMAKE_C_STANDARD {{default_c_standard}})
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_EXTENSIONS OFF)
 
-add_executable(${PROJECT_NAME} main.c)
+add_executable({{project_name}} main.c)
 ]]
-  write_file("CMakeLists.txt", content)
+  local current_vars = vim.tbl_deep_extend("force", {}, M.opts)
+  current_vars.project_name = get_safe_project_name()
+  local formatted_content = content:gsub("{{(.-)}}", function(key)
+    return tostring(current_vars[key] or "")
+  end)
+  write_file("CMakeLists.txt", formatted_content)
   vim.notify("Generated C17 CMakeLists.txt in " .. vim.fn.getcwd())
 end
 
 function M.setup(opts)
   M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
-  local aug = vim.api.nvim_create_augroup("CppPlug", { clear = true })
 
   vim.api.nvim_create_user_command("CMakeListsTxtGenCPP", gen_cpp, {})
   vim.api.nvim_create_user_command("CMakeListsTxtGenC", gen_c, {})
