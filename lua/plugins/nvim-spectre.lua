f21a1ab23ca1bfec69510d0bbe91dfde9a953b70
jamylak <jamylak@gmail.com>
2025-08-10 19:13:39 +1000

    nvim spectre keymap

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -2,6 +2,13 @@ return {
   "nvim-pack/nvim-spectre",
   lazy = true,
   cmd = "Spectre",
+  keys = {
+    {
+      "<leader>S",
+      "<cmd>Spectre<CR>",
+      desc = "Spectre: Search and Replace",
+    },
+  },
   opts = {
     mapping = {
       ['send_to_qf'] = {
