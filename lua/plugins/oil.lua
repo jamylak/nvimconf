ba3d2531a843c17244a6abea9b1c307ee1fd697e
jamylak <jamylak@gmail.com>
2025-08-11 22:47:58 +1000

    use oil keymaps to not interfere

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -55,44 +55,15 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   keys = {
-    { '<leader>od', '<CMD>Oil ' .. os.getenv 'HOME' .. '/.config/dotfiles<CR>', { desc = '[O]pen [D]otfiles' } },
-    { '<leader>ot', '<CMD>Oil /tmp<CR>', { desc = '[O]pen /[T]mp' } },
-    { '<leader>oc', '<CMD>Oil ' .. vim.fn.stdpath 'config' .. '<CR>', { desc = '[O]pen [N]eovim Config' } },
+    { '<leader>od', '<CMD>Oil ' .. os.getenv 'HOME' .. '/.config/dotfiles<CR>',   { desc = '[O]pen [D]otfiles' } },
+    { '<leader>ot', '<CMD>Oil /tmp<CR>',                                          { desc = '[O]pen /[T]mp' } },
+    { '<leader>oc', '<CMD>Oil ' .. vim.fn.stdpath 'config' .. '<CR>',             { desc = '[O]pen [N]eovim Config' } },
     { '<leader>on', '<CMD>Oil ' .. vim.fn.stdpath 'config' .. '/lua/plugins<CR>', { desc = '[O]pen [N]eovim Plugins Folder' } },
-    { '<leader>ob', '<CMD>Oil ' .. os.getenv 'HOME' .. '/bar<CR>', { desc = '[O]pen Projects' } },
-    { '<leader>op', '<CMD>Oil ' .. os.getenv 'HOME' .. '/proj<CR>', { desc = '[O]pen Projects' } },
-    { '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' } },
+    { '<leader>ob', '<CMD>Oil ' .. os.getenv 'HOME' .. '/bar<CR>',                { desc = '[O]pen Projects' } },
+    { '<leader>op', '<CMD>Oil ' .. os.getenv 'HOME' .. '/proj<CR>',               { desc = '[O]pen Projects' } },
+    { '-',          '<CMD>Oil<CR>',                                               { desc = 'Open parent directory' } },
 
-    { '<c-x><c-j>', '<CMD>Oil<CR>', { desc = 'Open parent directory' } },
-    {
-      'gp',
-      function()
-        -- if permissions is in columns then remove it to save space
-        if vim.tbl_contains(require('oil.config').columns, 'permissions') then
-          require('oil').set_columns { 'icon', 'size', 'mtime' }
-        else
-          require('oil').set_columns { 'permissions', 'icon', 'size', 'mtime' }
-        end
-      end,
-      { desc = 'Toggle of file permissions' },
-    },
-    {
-      '<leader>c',
-      function()
-        if require('oil.config').view_options.sort[2][1] == 'mtime' then
-          require('oil').set_sort {
-            { 'type', 'asc' },
-            { 'name', 'asc' },
-          }
-        else
-          require('oil').set_sort {
-            { 'type', 'desc' },
-            { 'mtime', 'desc' },
-          }
-        end
-      end,
-      { desc = '[C]hange sort order' },
-    },
+    { '<c-x><c-j>', '<CMD>Oil<CR>',                                               { desc = 'Open parent directory' } },
   },
   opts = {
     default_file_explorer = true,
@@ -145,6 +116,33 @@ return {
         end,
         desc = '[e]xa',
         mode = 'n',
+      },
+      ['gp'] = {
+        callback = function()
+          -- if permissions is in columns then remove it to save space
+          if vim.tbl_contains(require('oil.config').columns, 'permissions') then
+            require('oil').set_columns { 'icon', 'size', 'mtime' }
+          else
+            require('oil').set_columns { 'permissions', 'icon', 'size', 'mtime' }
+          end
+        end,
+        desc = 'Toggle of file permissions',
+      },
+      ['<leader>c'] = {
+        callback = function()
+          if require('oil.config').view_options.sort[2][1] == 'mtime' then
+            require('oil').set_sort {
+              { 'type', 'asc' },
+              { 'name', 'asc' },
+            }
+          else
+            require('oil').set_sort {
+              { 'type',  'desc' },
+              { 'mtime', 'desc' },
+            }
+          end
+        end,
+        desc = '[C]hange sort order',
       },
     },
     view_options = { show_hidden = true },
