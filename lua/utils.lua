93b4a01ff63744cfe22c99b96a7f482ca422313a
jamylak <jamylak@gmail.com>
2025-08-11 19:25:21 +1000

    adjust formatting

--- lua/utils.lua
+++ lua/utils.lua
@@ -109,74 +109,75 @@ end
 
 function M.fzfDir()
   -- TODO: /tmp and then it will do CD instead of git root?
-  local dirs = vim.fn.split(vim.fn.system [[ls -d /tmp ~/bar/* ~/proj/* ~/.config/dotfiles ~/.config/nvim 2>/dev/null]], '\n')
+  local dirs = vim.fn.split(vim.fn.system [[ls -d /tmp ~/bar/* ~/proj/* ~/.config/dotfiles ~/.config/nvim 2>/dev/null]],
+    '\n')
 
   require('telescope.pickers')
-    .new({}, {
-      prompt_title = 'Select Project',
-      finder = require('telescope.finders').new_table {
-        results = dirs,
-      },
-      sorter = require('telescope.config').values.generic_sorter {},
-      attach_mappings = function(_, map)
-        local function projTabFindFiles(path)
-          if path and path ~= '' then
-            if IsNonEmptyBuffer() then
-              vim.cmd 'tabnew'
-            end
-            vim.cmd('tcd ' .. path)
-            vim.cmd 'Telescope find_files'
-          end
-        end
-        map('i', '<CR>', function(prompt_bufnr)
-          local selection = require('telescope.actions.state').get_selected_entry()
-          require('telescope.actions').close(prompt_bufnr)
-          local path = selection[1]
-          projTabFindFiles(path)
-        end)
-        map('i', '<C-CR>', function(prompt_bufnr)
-          local selection = require('telescope.actions.state').get_selected_entry()
-          require('telescope.actions').close(prompt_bufnr)
-          local path = selection[1]
-          if path and path ~= '' then
-            local utils = require 'utils'
-            if not utils.switchToTabWithFile(path .. '/') then
-              projTabFindFiles(path)
+      .new({}, {
+        prompt_title = 'Select Project',
+        finder = require('telescope.finders').new_table {
+          results = dirs,
+        },
+        sorter = require('telescope.config').values.generic_sorter {},
+        attach_mappings = function(_, map)
+          local function projTabFindFiles(path)
+            if path and path ~= '' then
+              if IsNonEmptyBuffer() then
+                vim.cmd 'tabnew'
+              end
+              vim.cmd('tcd ' .. path)
+              vim.cmd 'Telescope find_files'
             end
           end
-        end)
-        map('i', '<C-j>', function(prompt_bufnr)
-          local selection = require('telescope.actions.state').get_selected_entry()
-          require('telescope.actions').close(prompt_bufnr)
-          local path = selection[1]
-          if path and path ~= '' then
-            local utils = require 'utils'
-            if not utils.switchToTabWithFile(path .. '/') then
-              projTabFindFiles(path)
+          map('i', '<CR>', function(prompt_bufnr)
+            local selection = require('telescope.actions.state').get_selected_entry()
+            require('telescope.actions').close(prompt_bufnr)
+            local path = selection[1]
+            projTabFindFiles(path)
+          end)
+          map('i', '<C-CR>', function(prompt_bufnr)
+            local selection = require('telescope.actions.state').get_selected_entry()
+            require('telescope.actions').close(prompt_bufnr)
+            local path = selection[1]
+            if path and path ~= '' then
+              local utils = require 'utils'
+              if not utils.switchToTabWithFile(path .. '/') then
+                projTabFindFiles(path)
+              end
             end
-          end
-        end)
-        map('i', '<S-CR>', function(prompt_bufnr)
-          local action_state = require 'telescope.actions.state'
-          local actions = require 'telescope.actions'
-          local input = action_state.get_current_line()
-          if input and input ~= '' then
-            local dir = vim.fn.expand('~/bar/' .. input)
-            actions.close(prompt_bufnr)
-            -- vim.schedule(function()
-            vim.fn.mkdir(dir, 'p')
-            if IsNonEmptyBuffer() then
-              vim.cmd 'tabnew'
+          end)
+          map('i', '<C-j>', function(prompt_bufnr)
+            local selection = require('telescope.actions.state').get_selected_entry()
+            require('telescope.actions').close(prompt_bufnr)
+            local path = selection[1]
+            if path and path ~= '' then
+              local utils = require 'utils'
+              if not utils.switchToTabWithFile(path .. '/') then
+                projTabFindFiles(path)
+              end
             end
-            vim.cmd('tcd ' .. dir)
-            vim.cmd 'Telescope find_files'
-            -- end)
-          end
-        end)
-        return true
-      end,
-    })
-    :find()
+          end)
+          map('i', '<S-CR>', function(prompt_bufnr)
+            local action_state = require 'telescope.actions.state'
+            local actions = require 'telescope.actions'
+            local input = action_state.get_current_line()
+            if input and input ~= '' then
+              local dir = vim.fn.expand('~/bar/' .. input)
+              actions.close(prompt_bufnr)
+              -- vim.schedule(function()
+              vim.fn.mkdir(dir, 'p')
+              if IsNonEmptyBuffer() then
+                vim.cmd 'tabnew'
+              end
+              vim.cmd('tcd ' .. dir)
+              vim.cmd 'Telescope find_files'
+              -- end)
+            end
+          end)
+          return true
+        end,
+      })
+      :find()
 end
 
 function M.lazygit()
