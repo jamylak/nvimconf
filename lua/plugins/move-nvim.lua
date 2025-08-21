86a4a039a9887e766fcbe419fae2d7a93318bdfa
jamylak <jamylak@gmail.com>
2025-08-21 22:14:02 +1000

    move nvim defaults when there are no lines

--- lua/plugins/move-nvim.lua
+++ lua/plugins/move-nvim.lua
@@ -1,12 +1,50 @@
 return {
   'fedepujol/move.nvim',
   keys = {
-    { '<C-j>', ':MoveBlock(1)<CR>', mode = 'v', desc = 'Move block down', silent = true },
-    { '<C-k>', ':MoveBlock(-1)<CR>', mode = 'v', desc = 'Move block up', silent = true },
-    -- { '<C-h>', ':MoveHBlock(-1)<CR>', mode = 'v', desc = 'Move block left', silent = true },
-    -- { '<C-l>', ':MoveHBlock(1)<CR>', mode = 'v', desc = 'Move block right', silent = true },
-    { '<C-j>', ':MoveLine(1)<CR>', mode = 'n', desc = 'Move block down', silent = true },
-    { '<C-k>', ':MoveLine(-1)<CR>', mode = 'n', desc = 'Move block up', silent = true },
+    { '<C-j>', ':MoveBlock(1)<CR>',  mode = 'v', desc = 'Move block down', silent = true },
+    { '<C-k>', ':MoveBlock(-1)<CR>', mode = 'v', desc = 'Move block up',   silent = true },
+    -- Give C-j a useful action when there are no lines to move eg.
+    -- Open file search etc
+    -- Also C-h to add some useful action outside in keymaps.lua?
+    -- Could also do for other things
+    {
+      '<C-j>',
+      function()
+        local line_count = vim.api.nvim_buf_line_count(0)
+        -- When there are no lines to move, open fzfDir
+        if line_count <= 1 then
+          require("fff").find_in_git_root()
+          -- if it had no git root
+          if vim.v.shell_error ~= 0 then
+            require("fff").find_files()
+          end
+        else
+          vim.cmd('MoveLine 1')
+        end
+      end,
+      mode = 'n',
+      desc = 'Move block down or fzfDir',
+      silent = true
+    },
+    -- Give <C-k> a useful action when there are no lines to move eg.
+    -- open terminal
+    {
+      '<C-k>',
+      function()
+        local line_count = vim.api.nvim_buf_line_count(0)
+        -- When there are no lines to move, open fzfDir
+        if line_count <= 1 then
+          -- Open terminal
+          vim.cmd('term')
+          vim.cmd('startinsert')
+        else
+          vim.cmd('MoveLine -1')
+        end
+      end,
+      mode = 'n',
+      desc = 'Move block up',
+      silent = true
+    },
   },
   opts = {
     line = {
