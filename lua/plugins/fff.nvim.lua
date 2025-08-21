5c9636fd2b39c29513a017f3b35d614276467475
jamylak <jamylak@gmail.com>
2025-08-21 19:28:49 +1000

    fff.nvim customisation

--- lua/plugins/fff.nvim.lua
+++ lua/plugins/fff.nvim.lua
@@ -1,10 +1,25 @@
 return {
   "dmtrKovalenko/fff.nvim",
   build = "cargo build --release",
+  cmd = {
+    "FFF",
+    "FFFFind",
+    "FFFScan",
+    "FFFRefreshGit",
+    "FFFClearCache",
+    "FFFHealth",
+    "FFFDebug",
+    "FFFOpenLog",
+  },
   -- or if you are using nixos
   -- build = "nix run .#release",
   opts = {
     -- pass here all the options
+    keymaps = {
+      close = { '<Esc>', '<C-c>' },
+      select = { '<CR>', '<C-j>', '<C-m>' },
+    }
+
   },
   keys = {
     {
