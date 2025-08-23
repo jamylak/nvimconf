26256da46709b238814e4ae7d27eeef5d3eb16cb
jamylak <jamylak@gmail.com>
2025-08-24 07:58:23 +1000

    crates.nvim

---
+++ lua/plugins/crates.nvim.lua
@@ -0,0 +1,17 @@
+return {
+  'saecki/crates.nvim',
+  tag = 'stable',
+  event = { "BufRead Cargo.toml" },
+  opts = {
+    lsp = {
+      enabled = true,
+      on_attach = function(client, bufnr)
+        -- the same on_attach function as for your other language servers
+        -- can be ommited if you're using the `LspAttach` autocmd
+      end,
+      actions = true,
+      completion = true,
+      hover = true,
+    },
+  },
+}
