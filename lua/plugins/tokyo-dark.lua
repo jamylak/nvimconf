079c2e44f2138f7be979944855650504f336b97f
jamylak <jamylak@gmail.com>
2025-08-25 13:28:24 +1000

    omments a bit lighter

--- lua/plugins/tokyo-dark.lua
+++ lua/plugins/tokyo-dark.lua
@@ -4,22 +4,23 @@ return {
   --   return vim.g.neovide
   -- end,
   -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  event = {'BufReadPost', 'BufWritePost'},
+  event = { 'BufReadPost', 'BufWritePost' },
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     local p = require 'tokyodark.palette'
     require('tokyodark').setup {
       styles = {
-        comments = { italic = true }, -- style for comments
-        keywords = { italic = false }, -- style for keywords
-        identifiers = { italic = false }, -- style for identifiers
-        functions = {}, -- style for functions
-        variables = {}, -- style for variables
+        comments = { italic = true },
+        keywords = { italic = false },
+        identifiers = { italic = false },
+        functions = {},
+        variables = {},
       },
       custom_highlights = {
         Terminal = { fg = p.white, bg = p.white },
         Visual = { bg = p.yellow, fg = p.black },
         StatusLineNC = { fg = p.white, bg = p.bg1, bold = true },
+        Comment = { fg = "#b0b0b0", italic = true },
       },
       transparent_background = true,
       -- terminal_colors = false,
