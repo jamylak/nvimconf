211cc052631d16d6bf595deefb3194a9a3c8637d
jamylak <jamylak@gmail.com>
2025-09-09 21:07:06 +1000

    actions preview quickfix

--- lua/plugins/actions-preview.lua
+++ lua/plugins/actions-preview.lua
@@ -9,4 +9,20 @@ return {
       end,
     },
   },
+  -- Quickfix for:
+  -- Unsupported layout_config key for the horizontal strategy: preview_width
+  -- use horizontal to not interfere with telescope default for
+  -- preview_width
+  opts = {
+    telescope = {
+      sorting_strategy = "ascending",
+      layout_strategy = "horizontal",
+      layout_config = {
+        width = 0.8,
+        height = 0.9,
+        prompt_position = "top",
+        preview_cutoff = 20,
+      },
+    },
+  },
 }
