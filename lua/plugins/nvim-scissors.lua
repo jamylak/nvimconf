39a985d00703570802e07adb60a960a9ec4ce42b
jamylak <jamylak@gmail.com>
2025-09-10 17:11:36 +1000

    nvim-scissors

---
+++ lua/plugins/nvim-scissors.lua
@@ -0,0 +1,29 @@
+local conf_dir = vim.fn.stdpath("config")
+local snippets_dir = conf_dir .. "/lua/snippets"
+
+return {
+  "chrisgrieser/nvim-scissors",
+  dependencies = "nvim-telescope/telescope.nvim",
+  opts = {
+    snippetDir = snippets_dir,
+  },
+  cmd = { "ScissorsAddNewSnippet", "ScissorsEditSnippet" },
+  keys = {
+    {
+      "<leader>se",
+      function() require("scissors").editSnippet() end,
+      desc = "[S]nippet - [E]dit"
+    },
+    {
+      "<leader>sa",
+      function() require("scissors").addNewSnippet() end,
+      desc = "[S]nippet - [A]dd"
+    },
+    {
+      "<leader>sa",
+      function() require("scissors").addNewSnippet() end,
+      desc = "[S]nippet - [A]dd",
+      mode = "x",
+    },
+  },
+}
