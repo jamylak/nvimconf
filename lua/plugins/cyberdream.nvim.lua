196d5136fe39eb6b304a29b883c6ced6fbbaec2f
jamylak <jamylak@gmail.com>
2025-09-13 06:14:31 +1000

    cyberdream cleanup

--- lua/plugins/cyberdream.nvim.lua
+++ lua/plugins/cyberdream.nvim.lua
@@ -1,7 +1,5 @@
 return {
   "scottmckendry/cyberdream.nvim",
-  -- lazy = false,
-  -- priority = 1000,
   event = { 'BufReadPost', 'BufWritePost' },
   config = function(_, opts)
     local p = require("cyberdream.colors")
@@ -10,6 +8,7 @@ return {
     require("cyberdream").setup({
       transparent = true,
       -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+      -- Currently it's having issues
       -- cache = true,
       highlights = {
         Visual = { bg = orange, fg = black },
