1a2ea313ea8a344f362846c943c0694173ddec84
jamylak <jamylak@gmail.com>
2025-09-12 20:48:06 +1000

    adjust cyberdream

--- lua/plugins/cyberdream.nvim.lua
+++ lua/plugins/cyberdream.nvim.lua
@@ -5,20 +5,22 @@ return {
   event = { 'BufReadPost', 'BufWritePost' },
   config = function(_, opts)
     local p = require("cyberdream.colors")
-    -- local orange = "#ffbd5e"
     local orange = "#efbd5e"
+    local black = "#000000"
     require("cyberdream").setup({
       transparent = true,
+      -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+      -- cache = true,
       highlights = {
+        Visual = { bg = orange, fg = black },
         -- Terminal = { fg = p.white, bg = p.white },
-        Visual = { bg = orange, fg = p.black },
         -- StatusLineNC = { fg = p.white, bg = p.bg1, bold = true },
         -- Comment = { fg = "#b0b0b0", italic = true },
         -- Comment = { fg = "#ffd8a8", italic = true },
       },
 
     })
-    vim.cmd [[ highlight Normal guifg=#FFFFFF ]]
+    -- vim.cmd [[ highlight Normal guifg=#000000 ]]
 
     vim.cmd.colorscheme "cyberdream"
   end,
