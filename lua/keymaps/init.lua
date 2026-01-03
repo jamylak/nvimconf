10e5ec56bb02ea8f78923bf205b85e31fe963517
jamylak <jamylak@gmail.com>
2026-01-03 21:17:27 +1100

    move some keymaps out

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,4 +1,5 @@
 local utils = require 'utils'
+require('keymaps.terminal').setup()
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
@@ -306,33 +307,6 @@ function ExecuteVisualSelectionAsLua()
   vim.api.nvim_win_set_cursor(0, save_cursor)
 end
 
-local function terminal()
-  vim.cmd 'term!'
-  vim.cmd 'startinsert'
-end
-local function terminalNewTab()
-  vim.cmd '-tabnew | term'
-  vim.cmd 'startinsert'
-end
-local function terminalVertical()
-  vim.cmd 'vsplit | term'
-  vim.cmd 'startinsert'
-end
-local function terminalHorizontal()
-  vim.cmd 'split | term'
-  vim.cmd 'startinsert'
-end
-vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New Tab' })
-
-vim.keymap.set('n', '<a-t>', ':split<CR><C-w>T', { desc = 'New Tab' })
-vim.keymap.set('n', '<c-t>', ':tabnew<CR>', { desc = 'New Tab' })
-vim.keymap.set('n', '<leader>te', terminal, { desc = ':term' })
-vim.keymap.set('n', '<leader>tk', terminal, { desc = ':term' })
-vim.keymap.set('n', '<leader>tt', terminalNewTab, { desc = 'Terminal - New Tab' })
-vim.keymap.set('n', '<S-CR>', terminalNewTab, { desc = 'Terminal - New Tab' })
-vim.keymap.set('n', '<leader>tv', terminalVertical, { desc = 'Terminal - Vertical' })
-vim.keymap.set('n', '<leader>tj', terminalVertical, { desc = 'Terminal - Vertical' })
-vim.keymap.set('n', '<leader>th', terminalHorizontal, { desc = 'Terminal - Horizontal' })
 vim.keymap.set('n', '<leader>tr', ':tabclose<CR>', { desc = 'Tab Remove' })
 vim.keymap.set('n', '<leader>tl', ':tablast<CR>', { desc = 'Tab Last' })
 vim.keymap.set('n', '<leader>tf', ':tabfirst<CR>', { desc = 'Tab First' })
