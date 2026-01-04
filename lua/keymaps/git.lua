f62bcd7ec86e53fdf476edd259248b59fd51b4ff
jamylak <jamylak@gmail.com>
2026-01-04 21:24:05 +1100

    combine git stuff

--- lua/keymaps/git.lua
+++ lua/keymaps/git.lua
@@ -1,3 +1,5 @@
+local utils = require 'utils'
+
 function GitHubURL()
   local utils = require 'utils'
   utils.cd_to_git_root()
@@ -30,5 +32,17 @@ function launchGitHubUrl()
   vim.fn.system('open ' .. url)
 end
 
+vim.api.nvim_create_user_command('CD', function()
+  utils.cd_to_git_root()
+end, {})
+
+vim.api.nvim_create_user_command('TCD', function()
+  utils.tcd_to_git_root()
+end, {})
+
+vim.keymap.set('n', '<leader>v', utils.tcd_to_git_root, { noremap = true })
+vim.keymap.set('n', '<leader>V', utils.cd_to_git_root, { noremap = true })
+vim.keymap.set('n', '<m-v>', utils.cd_to_git_root, { noremap = true })
+vim.keymap.set('n', '<leader>bc', utils.cd_to_git_root, { noremap = true })
 vim.keymap.set('n', '<leader><leader>G', CopyGitHubURLToClipboard, { desc = 'Copy GitHub URL' })
 vim.keymap.set('n', '<leader><leader>g', launchGitHubUrl, { desc = 'Launch GitHub URL' })
