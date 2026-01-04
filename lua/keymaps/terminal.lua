7e3854f79248bb4443e19e12be7363738bfe4923
jamylak <jamylak@gmail.com>
2026-01-04 21:43:52 +1100

    reorder

--- lua/keymaps/terminal.lua
+++ lua/keymaps/terminal.lua
@@ -18,51 +18,6 @@ local function terminalHorizontal()
   vim.cmd 'startinsert'
 end
 
-vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New Tab' })
-vim.keymap.set('n', '<a-t>', ':split<CR><C-w>T', { desc = 'New Tab' })
-vim.keymap.set('n', '<c-t>', ':tabnew<CR>', { desc = 'New Tab' })
-vim.keymap.set('n', '<leader>te', terminal, { desc = ':term' })
-vim.keymap.set('n', '<leader>tk', terminal, { desc = ':term' })
-vim.keymap.set('n', '<leader>tt', terminalNewTab, { desc = 'Terminal - New Tab' })
-vim.keymap.set('n', '<S-CR>', terminalNewTab, { desc = 'Terminal - New Tab' })
-vim.keymap.set('n', '<leader>tv', terminalVertical, { desc = 'Terminal - Vertical' })
-vim.keymap.set('n', '<leader>tj', terminalVertical, { desc = 'Terminal - Vertical' })
-vim.keymap.set('n', '<leader>th', terminalHorizontal, { desc = 'Terminal - Horizontal' })
-vim.keymap.set('n', '<C-\\>', function()
-  vim.cmd ':split | term'
-  vim.cmd 'startinsert'
-end, { silent = true, desc = 'Vertical Split' })
-vim.keymap.set('n', '<C-S-\\>', function()
-  vim.cmd ':vsplit | term'
-  vim.cmd 'startinsert'
-end, { silent = true, desc = 'Horizontal Split' })
-
-
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { silent = true })
-vim.keymap.set('t', '90w', '<C-W>', { silent = true })
-
-vim.api.nvim_set_keymap('n', '<leader><leader>v', ':vsplit | term fish -c "cb; rn;"<CR>',
-  { noremap = true, desc = '[c]make build and run vertical ' })
-
-vim.api.nvim_create_user_command('TT', terminalNewTab, {})
-vim.api.nvim_create_user_command('TV', terminalVertical, {})
-vim.api.nvim_create_user_command('TH', terminalHorizontal, {})
--- Custom command to start a new terminal with tmux attach
-vim.api.nvim_create_user_command('TA', function()
-  vim.cmd 'new | term tmux a'
-end, {})
-
--- Allow jk and ji escape in terminal mode but not in yazi or lazygit
-vim.api.nvim_create_autocmd('TermOpen', {
-  pattern = '*',
-  callback = function()
-    if not vim.api.nvim_buf_get_name(0):match 'yazi' and not vim.api.nvim_buf_get_name(0):match 'lazygit' then
-      vim.keymap.set('t', 'jk', '<C-\\><C-n>', { buffer = true, silent = true })
-      vim.keymap.set('t', 'ji', '<C-\\><C-n>', { buffer = true, silent = true })
-    end
-  end,
-})
-
 local function find_terminal_buffer_number()
   -- Get the current tabpage
   local tabpage = vim.api.nvim_get_current_tabpage()
@@ -133,6 +88,46 @@ local function sendTerminalRepeat(initialCommand)
   end
 end
 
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New Tab' })
+vim.keymap.set('n', '<a-t>', ':split<CR><C-w>T', { desc = 'New Tab' })
+vim.keymap.set('n', '<c-t>', ':tabnew<CR>', { desc = 'New Tab' })
+vim.keymap.set('n', '<leader>te', terminal, { desc = ':term' })
+vim.keymap.set('n', '<leader>tk', terminal, { desc = ':term' })
+vim.keymap.set('n', '<leader>tt', terminalNewTab, { desc = 'Terminal - New Tab' })
+vim.keymap.set('n', '<S-CR>', terminalNewTab, { desc = 'Terminal - New Tab' })
+vim.keymap.set('n', '<leader>tv', terminalVertical, { desc = 'Terminal - Vertical' })
+vim.keymap.set('n', '<leader>tj', terminalVertical, { desc = 'Terminal - Vertical' })
+vim.keymap.set('n', '<leader>th', terminalHorizontal, { desc = 'Terminal - Horizontal' })
+vim.keymap.set('n', '<C-\\>', function()
+  vim.cmd ':split | term'
+  vim.cmd 'startinsert'
+end, { silent = true, desc = 'Vertical Split' })
+vim.keymap.set('n', '<C-S-\\>', function()
+  vim.cmd ':vsplit | term'
+  vim.cmd 'startinsert'
+end, { silent = true, desc = 'Horizontal Split' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { silent = true })
+vim.keymap.set('t', '90w', '<C-W>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader><leader>v', ':vsplit | term fish -c "cb; rn;"<CR>',
+  { noremap = true, desc = '[c]make build and run vertical ' })
+
+vim.api.nvim_create_user_command('TT', terminalNewTab, {})
+vim.api.nvim_create_user_command('TV', terminalVertical, {})
+vim.api.nvim_create_user_command('TH', terminalHorizontal, {})
+-- Custom command to start a new terminal with tmux attach
+vim.api.nvim_create_user_command('TA', function()
+  vim.cmd 'new | term tmux a'
+end, {})
+-- Allow jk and ji escape in terminal mode but not in yazi or lazygit
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    if not vim.api.nvim_buf_get_name(0):match 'yazi' and not vim.api.nvim_buf_get_name(0):match 'lazygit' then
+      vim.keymap.set('t', 'jk', '<C-\\><C-n>', { buffer = true, silent = true })
+      vim.keymap.set('t', 'ji', '<C-\\><C-n>', { buffer = true, silent = true })
+    end
+  end,
+})
 vim.api.nvim_create_user_command('M', sendTerminalRepeat, {})
 -- TODO: Something where it will launch a Watch and Run below?
 -- vim.api.nvim_create_user_command('WatchAndRun', sendTerminalRepeat, {})
