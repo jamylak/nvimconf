c9ddc728869fb4055763588eea2f022374eb3888
jamylak <jamylak@gmail.com>
2026-01-04 21:22:09 +1100

    small fix

--- lua/keymaps/cwd.lua
+++ lua/keymaps/cwd.lua
@@ -1,3 +1,4 @@
+local function changeDirWindow()
   local file_path = vim.fn.expand '%:p'
   local dir_path = vim.fn.fnamemodify(file_path, ':h')
   vim.cmd('lcd ' .. dir_path)
@@ -9,7 +10,6 @@ local function changeDirTab()
   vim.cmd('tcd ' .. dir_path)
 end
 
--- vim.keymap.set('n', 'cd', changeDir, { desc = 'Change [C]urrent [D]irectory to parent of curfile' })
 vim.keymap.set('n', 'cd', changeDirTab, { desc = 'Tab Change [C]urrent [D]irectory to parent of curfile' })
 vim.keymap.set('n', '<leader>tc', changeDirTab, { desc = '[T]ab Change [C]urrent Directory to parent of curfile' })
 vim.keymap.set('n', '<leader>lc', changeDirWindow, { desc = 'Window Change [C]urrent Directory to parent of curfile' })
