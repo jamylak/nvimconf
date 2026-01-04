f8486c41b4f150780fc99c04b6b238699b728056
jamylak <jamylak@gmail.com>
2026-01-04 21:12:59 +1100

    cpp keymaps

---
+++ lua/keymaps/cpp.lua
@@ -0,0 +1,24 @@
+-- Switch from header file to source file and vice versa
+-- This is useful when you are working with C/C++ projects
+-- For example if i'm in include/abc.h it will go to src/abc.cpp
+-- If i'm in src/abc.cpp it will go to include/abc.h
+vim.api.nvim_create_user_command('S', function()
+  local file_path = vim.fn.expand '%:p:h'
+  local file_name = vim.fn.expand '%:t'
+  local new_file_path = ''
+  local new_file_name = ''
+  if file_path:match 'include' then
+    new_file_path = file_path:gsub('include', 'src')
+  elseif file_path:match 'src' then
+    new_file_path = file_path:gsub('src', 'include')
+  else
+    new_file_path = file_path
+  end
+  if file_name:match '%.cpp' then
+    new_file_name = file_name:gsub('%.cpp', '.h')
+  elseif file_name:match '%.h' then
+    new_file_name = file_name:gsub('%.h', '.cpp')
+  end
+  new_file_path = new_file_path .. '/' .. new_file_name
+  vim.cmd('e ' .. new_file_path)
+end, {})
--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -16,6 +16,7 @@ require 'keymaps.buffer_ops'
 require 'keymaps.motions'
 require 'keymaps.git_root'
 require 'keymaps.comments'
+require 'keymaps.cpp'
 
 
 -- TODO: Keymap for [TAB] in normal mode?
@@ -42,29 +43,3 @@ vim.keymap.set('i', 'jfd', '.', { silent = true })
 vim.keymap.set('i', 'jfg', '>', { silent = true })
 vim.keymap.set('i', 'jfq', '?', { silent = true })
 vim.keymap.set('i', 'jfs', '/', { silent = true })
-
-
--- Switch from header file to source file and vice versa
--- This is useful when you are working with C/C++ projects
--- For example if i'm in include/abc.h it will go to src/abc.cpp
--- If i'm in src/abc.cpp it will go to include/abc.h
-vim.api.nvim_create_user_command('S', function()
-  local file_path = vim.fn.expand '%:p:h'
-  local file_name = vim.fn.expand '%:t'
-  local new_file_path = ''
-  local new_file_name = ''
-  if file_path:match 'include' then
-    new_file_path = file_path:gsub('include', 'src')
-  elseif file_path:match 'src' then
-    new_file_path = file_path:gsub('src', 'include')
-  else
-    new_file_path = file_path
-  end
-  if file_name:match '%.cpp' then
-    new_file_name = file_name:gsub('%.cpp', '.h')
-  elseif file_name:match '%.h' then
-    new_file_name = file_name:gsub('%.h', '.cpp')
-  end
-  new_file_path = new_file_path .. '/' .. new_file_name
-  vim.cmd('e ' .. new_file_path)
-end, {})
