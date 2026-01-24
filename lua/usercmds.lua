fc6a052006858ff06fc00af7c126290666d8a46a
jamylak <jamylak@gmail.com>
2026-01-25 08:43:24 +1100

    add `FindWord` user cmd

--- lua/usercmds.lua
+++ lua/usercmds.lua
@@ -10,3 +10,9 @@ vim.api.nvim_create_user_command('FindFiles', function()
     vim.cmd 'Telescope find_files'
   end)
 end, { desc = 'Open Telescope find_files after UI is ready' })
+
+vim.api.nvim_create_user_command('FindWord', function()
+  vim.schedule(function()
+    require('telescope.builtin').live_grep()
+  end)
+end, { desc = 'Open Telescope live_grep after UI is ready' })
